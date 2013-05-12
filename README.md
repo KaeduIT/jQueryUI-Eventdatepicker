661748ffbbd5b143387404f6f941b31573ad6cd6
it@kaedu.co <it@kaedu.co>
2013-05-12 06:09:45 +0100

    namespace, data API

--- README.md
+++ README.md
@@ -3,7 +3,25 @@ jQueryUI-Eventdatepicker
 
 Introduction
 ------------
-This is a demo application using the jQueryUI Datepicker extension, jQueryUI Eventdatepicker.
-Adds events to the datepicker and uses datasource to perform crud operations on individual events.
+The jQueryUI Eventdatepicker is an extension of the jQueryUI Datepicker.
+This widget allows users to add events to the datepicker. 
+Events can be edited if a datasource is specified.
 
+## Demo
+[Demo Eventdatepicker](http://kaeduit.github.io/jQueryUI-Eventdatepicker)
 
+## Download
+* [Download the latest zip](https://github.com/KaeduIT/jQueryUI-Eventdatepicker/zipball/master).
+* [Download the latest tar](https://github.com/KaeduIT/jQueryUI-Eventdatepicker/tarball/master).
+* Clone the repo: `git clone git://github.com/KaeduIT/jQueryUI-Eventdatepicker.git`.
+
+## Features
+* **Events Display:**  
+  Allows display of events listed from a read only source.
+* **Events Read/Write:**  
+  Allows create/read/update/delete of events to a configured datasource. Access the Eventdatepicker event details via contextmenu.
+* **Update the events indicator:**  
+  Shows an image on dates with events, event indicator image can be changed.
+
+## Browsers
+The Eventdatepicker is in testing across the latest browser versions.
--- server/data.php
+++ server/data.php
@@ -1,11 +1,34 @@
 <?php
 
+
 switch($_POST['Source'])
 {
-	case "DataBase":
+	case 'DataBase':
+		$edd = new EventDatabaseData('database.config.php');
+		switch($_POST['CRUD'])
+		{
+			case 'create':
+				parse_str(trim($_POST['FormData']['Data'],"[]"));
+				$edd->createData(htmlentities($DiaryId,ENT_NOQUOTES,"UTF-8"), htmlentities($EventName,ENT_NOQUOTES,"UTF-8"), htmlentities($EventDesc,ENT_NOQUOTES,"UTF-8"), htmlentities($EventDate,ENT_NOQUOTES,"UTF-8"), htmlentities($IsAnnual,ENT_NOQUOTES,"UTF-8"));
+			break;
+			case 'read':
+				$edd->readData($_POST['id']);
+			break;
+			case 'update':
+				parse_str(trim($_POST['FormData']['Data'],"[]"));
+				$edd->updateData(htmlentities($EventId,ENT_NOQUOTES,"UTF-8"), htmlentities($EventName,ENT_NOQUOTES,"UTF-8"), htmlentities($EventDesc,ENT_NOQUOTES,"UTF-8"), htmlentities($EventDate,ENT_NOQUOTES,"UTF-8"), htmlentities($IsAnnual,ENT_NOQUOTES,"UTF-8"));
+			break;
+			case 'delete':
+				parse_str(trim($_POST['FormData']['Data'],"[]"));
+				$edd->deleteData(htmlentities($EventId,ENT_NOQUOTES,"UTF-8"));
+			break;
+			default:
+				$edd->readData($_POST['id']);
+			break;
+		}
+		break;
 	default:
-			$edd = new EventDatabaseData();
-			$edd->getData();
+			$edd->readData($_POST['id']);
 		break;
 }
 
@@ -20,7 +43,10 @@ abstract class EventData
         return $this->$member;
     }
 
-	abstract protected function getData();
+	abstract protected function createData($DiaryId, $EventName, $EventDesc, $EventDate, $IsAnnual);
+	abstract protected function readData($DiaryId);
+	abstract protected function updateData($EventId, $EventName, $EventDesc, $EventDate, $IsAnnual);
+	abstract protected function deleteData($EventId);
 //	{
 //		$index = array();
 //		$index[0]["EventDate"] = "1 April 2013";
@@ -36,10 +62,10 @@ class EventDatabaseData extends EventData
 {
 	protected $pdo;
 	protected $config;
-	function __construct()
+	function __construct($file)
 	{
 		parent::__construct();
-		$this->config 	= include 'database.config.php';
+		$this->config 	= include $file;
 		$this->pdo 		= new PDO($this->config['connection_properties']['connection']['dsn'], $this->config['connection_properties']['connection']['username'], $this->config['connection_properties']['connection']['password'], array(PDO::ATTR_PERSISTENT => true) );
 	}
 
@@ -48,20 +74,70 @@ class EventDatabaseData extends EventData
        //$this->pdo = null;
     }
 
-	function getData()
+	function createData($DiaryId, $EventName, $EventDesc, $EventDate, $IsAnnual)
 	{
 		try
 		{
-			$query = $this->pdo->prepare("SELECT DATE_FORMAT(EventDate,'%e %M %Y') EventDate, EventDesc, IsAnnual FROM event WHERE DiaryId=".$_POST['id']);
+			$query = $this->pdo->prepare("INSERT INTO event (EventName, EventDesc, EventDate, IsAnnual) VALUES ('".$EventName."', '".$EventDesc."', DATE_FORMAT(".$EventDate.",'%e %M %Y'), ".$IsAnnual.") WHERE DiaryId=".$DiaryId);
+			echo $query->execute();
+			//$rows  = $query->fetchAll(PDO::FETCH_ASSOC);
+			//echo json_encode($rows);
+		}
+		catch (PDOException $e)
+		{
+			echo 'Create failed: ' . $e->getMessage();
+		}
+	}
+
+	function readData($DiaryId)
+	{
+		try
+		{
+			$query = $this->pdo->prepare("SELECT EventId, EventName, EventDesc, DATE_FORMAT(EventDate,'%e %M %Y') EventDate, IsAnnual FROM event WHERE DiaryId=".$DiaryId);
 			$query->execute();
 			$rows  = $query->fetchAll(PDO::FETCH_ASSOC);
 			echo json_encode($rows);
 		}
 		catch (PDOException $e)
 		{
-			echo 'Connection failed: ' . $e->getMessage();
+			echo 'Select failed: ' . $e->getMessage();
 		}
 	}
+
+	function updateData($EventId, $EventName, $EventDesc, $EventDate, $IsAnnual)
+	{
+		try
+		{
+			$query = $this->pdo->prepare("UPDATE event SET EventName='".$EventName."', EventDesc='".$EventDesc."', EventDate='".$EventDate."', IsAnnual=".$IsAnnual." WHERE EventId=".$EventId);
+			echo $query->execute();
+			//$rows  = $query->fetchAll(PDO::FETCH_ASSOC);
+			//echo json_encode($rows);
+		}
+		catch (PDOException $e)
+		{
+			echo 'Update failed: ' . $e->getMessage();
+		}
+	}
+
+	function deleteData($EventId)
+	{
+		try
+		{
+			// should never happen from UI
+			if ( strlen($EventId) <=  0)
+				echo 'Delete failed, no event to delete';
+
+			$query = $this->pdo->prepare("DELETE FROM event WHERE EventId=".$EventId);
+			echo $query->execute();
+			//$rows  = $query->fetchAll(PDO::FETCH_ASSOC);
+			//echo json_encode($rows);
+		}
+		catch (PDOException $e)
+		{
+			echo 'Delete failed: ' . $e->getMessage();
+		}
+	}
+
 }
 
 ?>
